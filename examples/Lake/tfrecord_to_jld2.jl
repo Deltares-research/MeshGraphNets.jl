7102bcd5fc8053184e54d4d2be3b5ba77af618c4
WOTrompDeltares <willem.tromp@deltares.nl>
2026-02-13 13:09:58 +0100

    include edges in tfrecord_to_jld2

--- examples/Lake/tfrecord_to_jld2.jl
+++ examples/Lake/tfrecord_to_jld2.jl
@@ -10,6 +10,11 @@ features = Dict{String, Dict}(
         "shape" => [1, -1, 3],
         "dtype" => "int32"
     ),
+    "edges" => Dict{String, Dict}(
+        "type" => "static",
+        "shape" => [1, -1, 2],
+        "dtype" => "int32",
+    ),
     "mesh_pos" => Dict{String, Any}(
         "type" => "static",
         "shape" => [1, -1, 2],
@@ -40,15 +45,17 @@ features = Dict{String, Dict}(
 function parse_data(data::TFRecord.Example)
     out = Dict{String, AbstractArray}()
     for (key, value) in features
-        d = reinterpret(getfield(Base, Symbol(uppercasefirst(value["dtype"]))),
-            data.features.feature[key].kind.value.value[])
-        dims = Tuple(reverse(replace(
-            value["shape"], -1 => abs(reduce(div, value["shape"]; init = length(d))))))
-        d = reshape(d, dims)
-        if value["type"] == "static"
-            d = repeat(d, 1, 1, trajectory_length)
+        if key in keys(data.features.feature)
+            d = reinterpret(getfield(Base, Symbol(uppercasefirst(value["dtype"]))),
+                data.features.feature[key].kind.value.value[])
+            dims = Tuple(reverse(replace(
+                value["shape"], -1 => abs(reduce(div, value["shape"]; init = length(d))))))
+            d = reshape(d, dims)
+            if value["type"] == "static"
+                d = repeat(d, 1, 1, trajectory_length)
+            end
+            out[key] = d
         end
-        out[key] = d
     end
     return out
 end
@@ -60,14 +67,22 @@ for file in ["train", "valid", "test"]
         traj_dict = parse_data(traj)
         traj_group = JLD2.Group(jld2_file, "trajectory_$i")
 
-        cells = traj_dict["cells"]
+        if "cells" in keys(traj_dict)
+            cells = traj_dict["cells"]
+            traj_group["cells"] = cells[:, :, 1]
+        elseif "edges" in keys(traj_dict)
+            edges = traj_dict["edges"]
+            traj_group["edges"] = edges[:,:,1]
+        else
+            @err "Neither cells nor edges found in file data/$file.tfrecord"
+        end
+
         mesh_pos = traj_dict["mesh_pos"]
         node_type = traj_dict["node_type"]
         bathymetry = traj_dict["bathymetry"]
         waterlevel = traj_dict["waterlevel"]
         velocity = traj_dict["velocity"]
 
-        traj_group["cells"] = cells[:, :, 1]
         for idx in axes(mesh_pos, 2)
             traj_group["node[$idx].mesh_pos"] = mesh_pos[:, idx, 1]
             traj_group["node[$idx].node_type"] = node_type[:, idx, 1]
